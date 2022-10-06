ab567eb7e1fd2210e2aea6445521c91692970286
Drew Walter <mist3rwalter@gmail.com>
2022-10-06 16:01:08 -0400

    Bump gradescope base image to Ubuntu 22.04

--- make-gradescope.sh
+++ make-gradescope.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sed 's|FROM debian:stable-slim|FROM gradescope/auto-builds:ubuntu-20.04|g' Dockerfile > Dockerfile-gradescope
+sed 's|FROM debian:stable-slim|FROM gradescope/auto-builds:ubuntu-22.04|g' Dockerfile > Dockerfile-gradescope
 export LOCAL_IMAGE_NAME=acl2_gradescope_autograder
 export REMOTE_IMAGE_NAME=atwalter/acl2_gradescope_autograder
 export DOCKERFILE=Dockerfile-gradescope
+export IMAGE_VERSION=cs2800fa22
 
 make build
 make push
