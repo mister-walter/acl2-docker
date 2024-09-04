b601f0985753cc11d53f7a10e402bcafed232646
Drew Walter <mist3rwalter@gmail.com>
2024-09-04 14:14:29 -0400

    Update Gradescope dockerfile
    The Gradescope dockerfile needs to be a bit different because the
    version of SBCL packaged for Ubuntu 22.04 is unable to build the latest
    SBCL from source.

--- make-gradescope.sh
+++ make-gradescope.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sed 's|FROM debian:stable-slim|FROM gradescope/auto-builds:ubuntu-22.04|g' Dockerfile > Dockerfile-gradescope
+#sed 's|FROM debian:stable-slim|FROM gradescope/autograder-base:ubuntu-22.04|g' Dockerfile > Dockerfile-gradescope
 export LOCAL_IMAGE_NAME=acl2_gradescope_autograder
 export REMOTE_IMAGE_NAME=atwalter/acl2_gradescope_autograder
 export DOCKERFILE=Dockerfile-gradescope
-export IMAGE_VERSION=cs2800fa23
+export IMAGE_VERSION=cs2800fa24
 
-make build ACL2_COMMIT=a718c3aab01cc2980978136356ca5f9474ea5c94
+make build ACL2_COMMIT=1a6eb5cd12d6ed4982e2a5ba9614169ce74af0da
 make push
-rm Dockerfile-gradescope
+#rm Dockerfile-gradescope
